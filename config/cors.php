ac0e1b7b980739b87c1f32012a40dd67ff5aa389
abdurehmannazer <abdurehmannazer@gmail.com>
2025-10-04 23:08:43 +0300

    edit backend

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], // React app URL
+    'allowed_origins' => ['http://localhost:3000'], // React app URL
 
     'allowed_origins_patterns' => [],
 
